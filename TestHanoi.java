8077c02743eceebf3f93745b31d93ca71c60dde6
Sean Chambers <sean.schambers@gmail.com>
2022-11-06 23:08:26 -0800

    Update TestHanoi.java
    useful comments

--- TestHanoi.java
+++ TestHanoi.java
@@ -9,7 +9,8 @@ public class TestHanoi {
         col1.add(new Disk(2));
         col1.add(new Disk(1));
 
-        solveHanoi(4,col1,col3,col2); //not sure how to clean this so col3 is last
+        //this represents a a request to move 4 disks from col1 to col3
+        solveHanoi(4,col1,col3,col2); 
 
         System.out.println(col3.remove());
         System.out.println(col3.remove());
@@ -18,12 +19,16 @@ public class TestHanoi {
     }
 
     public static void solveHanoi(int n, Peg source, Peg destination, Peg other){
+        //base case if only one disk left to move, then move to destination
         if(n==1){
             destination.add(source.remove());
             return;
         } else {
+            //recursively move one less disk to open peg to allow bottom disk to move
             solveHanoi(n-1, source, other, destination); 
+            //move bottom peg to destination
             destination.add(source.remove());
+            //recursively move the stack of one less disk to be on top of recently moved disk.
             solveHanoi(n-1, other, destination, source);
         }
     }
